fe77a547d5d07f9f5a4c6f6b552bd5d720e5c7a3
kaitongg <kaitongg@andrew.cmu.edu>
2026-01-10 12:06:02 -0500

    Initial Commit

---
+++ SolarRise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SolarRise
+//
+//  Created by gktnbjl on 2026/1/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SolarRise/SolarRiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarRiseApp.swift
+//  SolarRise
+//
+//  Created by gktnbjl on 2026/1/10.
+//
+
+import SwiftUI
+
+@main
+struct SolarRiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
